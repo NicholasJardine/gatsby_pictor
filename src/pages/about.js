ec263b494320c845aeecc82a52730b6b83c75f36
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-01-26 13:42:05 +0200

    channged the content

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,6 +17,9 @@ const Paragraph = styled.div`
     p{
         font-size:16px;
         font-weight:500;
+        &:nth-child(1){
+            margin-top:40px;
+        }
     }
 `
 
@@ -27,13 +30,7 @@ const AboutPage = () => (
 
 Pictor allows production companies or casting agents to upload their Casting briefs and scripts directly on the platform, with the ability to specify race, gender, average age, language, skill sets, body type, location and more on the briefs, sending them directly to actors whose profiles match the specified character description. The actors can then submit their self-made casting tapes directly to production companies, via our website. Thus saving both the actor and the company time and money.</p></Paragraph>
 <Paragraph> Additionally, as actors create profiles, Pictor grows as a database for South African actors, making it easier to find the agents that the actors are with or contacting them directly, should they be open to that. Pictor also keeps it’s readers up to date with what’s happening in the industry.</Paragraph>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
   </Layout>
 )
 
