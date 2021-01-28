bc7c6963af8eb6f55c01b0e130c2fea9445e2305
Stuart <robinjamesauthor@gmail.com>
2021-01-28 19:53:41 +0200

    Added footer and removed redundant code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,23 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Navbar from "./ui/navbar"
-import Header from "./header"
-import "./layout.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './ui/footer'
+import styled from "styled-components"
+
+const Body = styled.html`
+  margin: 0;
+`
 
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-
+    <Body>
       <Navbar></Navbar>
-      <div
-        style={{
-          // margin: `0 auto`,
-          maxWidth: 960,
-          // padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
-
       </div>
-    </>
+      <Footer />
+    </Body>
   )
 }
 
